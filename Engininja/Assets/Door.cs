3a8cd3bd7ff49f6e41d2d4bbe5f33c3405c3d9f1
Jamie Young <jamiegyoung22@gmail.com>
2023-02-21 22:52:34 +0000

    Fix door shadows when open

--- Engininja/Assets/Door.cs
+++ Engininja/Assets/Door.cs
@@ -1,22 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Rendering.Universal;
 
 public class Door : MonoBehaviour, Interactable
 {
     private Animator anim;
     private Collider2D coll;
+    private ShadowCaster2D shadowCaster;
 
     private void Start()
     {
         anim = GetComponent<Animator>();
         coll = GetComponent<Collider2D>();
+        shadowCaster = GetComponent<ShadowCaster2D>();
     }
 
     public void Interact()
     {
         anim.SetBool("isOpen", !anim.GetBool("isOpen"));
         coll.isTrigger = !coll.isTrigger;
+        shadowCaster.selfShadows = !shadowCaster.selfShadows;
+        shadowCaster.castsShadows = !shadowCaster.castsShadows;
     }
 
 }
