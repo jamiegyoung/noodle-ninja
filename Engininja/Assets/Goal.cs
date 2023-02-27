e352001a4bc155d169088c9893ad78e6fb987164
Jamie Young <jamiegyoung22@gmail.com>
2023-02-27 19:53:53 +0000

    Fix ending

--- Engininja/Assets/Goal.cs
+++ Engininja/Assets/Goal.cs
@@ -8,16 +8,26 @@ public class Goal : MonoBehaviour
 {
     public GameObject player;
     private PlayableDirector dir;
+    public AudioSource globalAudioSource;
+    private bool volumeDownFlag = false;
     void Start()
     {
         dir = GetComponent<PlayableDirector>();
+    }
+
+    private void FixedUpdate()
+    {
+        if (volumeDownFlag)
+        {
+            globalAudioSource.volume = globalAudioSource.volume - 0.01f;
+        }
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject == player)
         {
-            Debug.Log("coll");
+            volumeDownFlag = true;
             dir.Play();
         }
     }
